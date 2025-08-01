ece0269eb820c9a2a33ffcef84bb2ed195e4c50f
bheibz <59157263+bheibz@users.noreply.github.com>
2025-08-01 13:12:16 +0700

    Update index.js

--- bebz-chat-backend/index.js
+++ bebz-chat-backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 8080;
-require("dotenv").config();
 
 app.use(express.json());
 
